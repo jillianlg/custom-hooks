ec487d5b9508db63965dd97108c44cc41df714a1
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 15:44:48 -0800

    adds listofCharacters to test, starts and passes propTypes to Character component

---
+++ src/components/Character.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Character = ({ }) => (
+ 
+);
+
+Character.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+};
+
+export default Character;
--- src/containers/CharacterPages.test.jsx
+++ src/containers/CharacterPages.test.jsx
@@ -3,9 +3,11 @@ import { render, screen, waitFor } from '@testing-library/react';
 import CharacterPage from './CharacterPages';
 
 describe('CharacterPages container', () => {
-  it('displays a loading screen then a list of characters', () => {
+  it('displays a loading screen then a list of characters', async() => {
     render(<CharacterPage />);
 
     screen.getByAltText('loading');
+
+    const listOfCharacters = await screen.findByTestId('characters')
   });
 });
