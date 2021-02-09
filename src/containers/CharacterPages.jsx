c47e6f477db8250223b88d4876dbd1f71b220cc4
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 15:01:44 -0800

    adds render to CharacterPage componet and test

--- src/containers/CharacterPages.jsx
+++ src/containers/CharacterPages.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 export default class CharacterPage extends Component {
   render() {
-    
+    return (
+      <h1>Character Page</h1>
+    );
   }
 }
--- src/containers/CharacterPages.test.jsx
+++ src/containers/CharacterPages.test.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import CharacterPage from './CharacterPages';
 
 describe('CharacterPages container', () => {
-  it('displays a loading screen then a list of characters', async() => {
-
+  it('displays a loading screen then a list of characters', () => {
+    render(<CharacterPage />);
   });
 });
