b392900bfb3cd81d8112b5d5faf3ec8a3a4351eb
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 15:45:39 -0800

    passes props to Character componet - test fails as expected

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Character = ({ }) => (
- 
+const Character = ({ image, name }) => (
+  <figure>
+    <img src={image} alt={name} />
+    <figcaption>{name}</figcaption>
+  </figure>
 );
 
 Character.propTypes = {
