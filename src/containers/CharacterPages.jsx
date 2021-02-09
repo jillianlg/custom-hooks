805f2661da8d68831c92110efcd5b5e3d257c8ec
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 15:36:18 -0800

    updates loading spinner with working link and rendersin browser - test passes as expected

--- src/containers/CharacterPages.jsx
+++ src/containers/CharacterPages.jsx
@@ -8,7 +8,7 @@ export default class CharacterPage extends Component {
   render() {
     const { loading } = this.state;
 
-    if(loading) return <img src="https://tenor.com/view/rick-and-morty-rest-and-ricklaxation-crying-sobbing-exhausted-gif-9601871" alt="loading" />;
+    if(loading) return <img src="https://media.giphy.com/media/cLqtYAKCLI41ArIC48/giphy.gif" alt="loading" />;
     return (<h1>Character Page</h1>);
   }
 
