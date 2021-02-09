249ef335abbb506a5a539aa874a4906008227c69
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 14:46:03 -0800

    import react-router-dom, update App.js with Router, Switch and Route, and remove App.test.js

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component="" />
+        </Switch>
+      </Router>
+    </>
+  );
+}
