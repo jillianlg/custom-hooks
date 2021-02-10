f41aef6de881f4335b04392695410d18f65364df
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 18:42:43 -0800

    REFACTOR w/ HOOKS - updates loading state

--- src/containers/CharacterPages.jsx
+++ src/containers/CharacterPages.jsx
@@ -1,29 +1,38 @@
 /* eslint-disable max-len */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CharacterList from '../components/characters/CharacterList';
 import Loading from '../components/loading/Loading';
 import { findCharacters } from '../services/RickAndMortyApi';
 
-export default class CharacterPage extends Component {
-  state = {
-    loading: true,
-    characters: []
-  }
+const CharacterPage = () => {
+  const { loading, setLoading } = useState;
 
-  componentDidMount() {
-    findCharacters()
-      .then(characters => {
-        this.setState({ characters, loading: false });
-      });
-  }
+  if(loading) return <Loading />;
+};
 
-  render() {
-    const { loading, characters } = this.state;
 
-    if(loading) return <Loading />;
-    return (
-      <CharacterList characters={characters} />
-    );
-  }
+export default CharacterPage;
 
-}
+// export default class CharacterPage extends Component {
+//   state = {
+//     loading: true,
+//     characters: []
+//   }
+
+//   componentDidMount() {
+//     findCharacters()
+//       .then(characters => {
+//         this.setState({ characters, loading: false });
+//       });
+//   }
+
+//   render() {
+//     const { loading, characters } = this.state;
+
+//     if(loading) return <Loading />;
+//     return (
+//       <CharacterList characters={characters} />
+//     );
+//   }
+
+// }
