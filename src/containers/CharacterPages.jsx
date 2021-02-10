d5ef22a204632062d77073a83d9bb8fd79a84c93
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 18:51:06 -0800

    REFACTOR w/ HOOKS - updates character state, converted componetDidMount to useEffects and passed down state with fetch call

--- src/containers/CharacterPages.jsx
+++ src/containers/CharacterPages.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CharacterList from '../components/characters/CharacterList';
 import Loading from '../components/loading/Loading';
 import { findCharacters } from '../services/RickAndMortyApi';
 
 const CharacterPage = () => {
-  const { loading, setLoading } = useState;
+  const [loading, setLoading]  = useState;
+  const [characters, setCharacters] = useState;
+
+  useEffect(() => {
+    findCharacters()
+      .then(characters => {
+        setCharacters(characters);
+        setLoading(false);
+      });
+  }, []);
 
   if(loading) return <Loading />;
+
 };
 
 
