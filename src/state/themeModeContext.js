7d31668a9587bff423581eb191f56fd15e05fa9a
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-10 14:57:02 -0800

    removes uesTheme file with dead code, moves useTheme code to themeModeContext, fixes errors

--- src/state/themeModeContext.js
+++ src/state/themeModeContext.js
@@ -1,14 +1,19 @@
-import React, { createContext } from 'react';
-import Loading from '../components/loading/Loading';
+import React, { createContext, useContext, useState } from 'react';
 
 const themeModeContext = createContext(null);
 
 export const ThemeModeProvider = ({ children }) => {
-  const { loading, themeMode } = useTheme();
+  const [themeMode, setThemeMode] = useState('light');
 
   return (
-    <themeModeContext.Provider value={{ children }}>
-      {loading ? <Loading /> : children}
+    <themeModeContext.Provider value={{ themeMode }}>
+      {children}
     </themeModeContext.Provider>
   );
+};
+
+export const useTheme = () => {
+  const { themeMode, setThemeMode } = useContext(themeModeContext);
+  
+  return themeMode;
 };
