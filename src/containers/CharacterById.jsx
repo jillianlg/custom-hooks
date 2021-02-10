ed860588992f7f6c9c9026a982b4aa7e9bc15854
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 20:03:33 -0800

    REFACTOR w/ CUSTOM HOOKS - reamove dead code and test passes as expected

--- src/containers/CharacterById.jsx
+++ src/containers/CharacterById.jsx
@@ -1,23 +1,12 @@
 /* eslint-disable max-len */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CharacterDetail from '../components/details/CharacterDetails';
 import Loading from '../components/loading/Loading';
-import { findCharacterById } from '../services/RickAndMortyApi';
 import { useCharacterById } from '../hooks/characters';
 
 const CharacterById = ({ match }) => {
   const { loading, character } = useCharacterById(match.params.id);
-  // const [loading, setLoading] = useState(true);
-  // const [character, setCharacter] = useState(null);
-
-  // useEffect(() => {
-  //   findCharacterById(match.params.id)
-  //     .then(character => {
-  //       setCharacter(character);
-  //       setLoading(false);
-  //     });
-  // }, []);
 
   if(loading) return <Loading />;
   return <CharacterDetail {...character} />;
