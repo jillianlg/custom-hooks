e8f61d7e01dfa354cc956e138f039fdd723c88d5
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 17:56:34 -0800

    adds PropTypes, match, params to CharacterById class component

--- src/containers/CharacterById.jsx
+++ src/containers/CharacterById.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable max-len */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CharacterDetail from '../components/details/CharacterDetails';
 import Loading from '../components/loading/Loading';
 
 export default class CharacterById extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.string.isRequired
+      }).isRequired
+    }).isRequired
+  }
+  
   state = {
     loading: true,
     character: null
