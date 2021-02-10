04f14376e49651e9914960829a0c814a694d7fac
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 19:49:51 -0800

    REFACTOR w/ CUSTOM HOOKS - move useEffects functions to characters.js from CharacterPages.jsx

--- src/hooks/characters.js
+++ src/hooks/characters.js
@@ -1,8 +1,17 @@
 import { useEffect, useState } from 'react';
+import { findCharacters } from '../services/RickAndMortyApi';
 
 export const useCharacters = () => {
   const [loading, setLoading]  = useState(true);
   const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    findCharacters()
+      .then(characters => {
+        setCharacters(characters);
+        setLoading(false);
+      });
+  }, []);
 
   return {
     loading,
