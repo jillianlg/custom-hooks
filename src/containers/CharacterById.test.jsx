40b0411a92404fd6a5687c1f27739248e88ad6a8
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 18:10:40 -0800

    adds mock API to test with msw, adds CharacterById.json to fixtures for mock testing

--- src/containers/CharacterById.test.jsx
+++ src/containers/CharacterById.test.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import characterByIdResponse from '../fixtures/characterById.json';
 import CharacterById from './CharacterById';
 
+const server = setupServer(
+  rest.get('https://rickandmortyapi.com/api/character/2', (req, res, ctx) => {
+    return res(ctx.json(characterByIdResponse));
+  })
+);
+
 describe('CharacterById container', () => {
-  it('fetches and displays a list of character details', async() => {
-    render(<CharacterById match={{ params: { id: '2' } }}/>);
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('fetches and displays a characters details', async() => {
+    render(<CharacterById match={{ params: { id: '2' } }} />);
 
     screen.getByAltText('loading');
-
+    
     return waitFor(() => {
       screen.getByText('Morty Smith');
       screen.getByText('Human');
