0434cd1820496d7e743debced029d2e998c4c651
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 19:10:13 -0800

    REFACTOR w/ HOOKS - passes match as props, adds loading and character state with useState

--- src/containers/CharacterById.jsx
+++ src/containers/CharacterById.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable max-len */
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import PropTypes from 'prop-types';
 import CharacterDetail from '../components/details/CharacterDetails';
 import Loading from '../components/loading/Loading';
 import { findCharacterById } from '../services/RickAndMortyApi';
 
-const CharacterById = () => {
-
+const CharacterById = ({ match }) => {
+  const [loading, setLoading] = useState(true);
+  const [character, setCharacter] = useState(null);
 };
 
 CharacterById.propTypes = {
