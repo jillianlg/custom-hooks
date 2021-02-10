e498be6d0bbc4db84cdbef48bac7e03a671f11cb
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 20:02:02 -0800

    REFACTOR w/ CUSTOM HOOKS - import useCharacterById custom hook into CharacterById.jsx and pass in match.param.id

--- src/containers/CharacterById.jsx
+++ src/containers/CharacterById.jsx
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 import CharacterDetail from '../components/details/CharacterDetails';
 import Loading from '../components/loading/Loading';
 import { findCharacterById } from '../services/RickAndMortyApi';
+import { useCharacterById } from '../hooks/characters';
 
 const CharacterById = ({ match }) => {
+  const { loading, character } = useCharacterById(match.params.id);
   // const [loading, setLoading] = useState(true);
   // const [character, setCharacter] = useState(null);
 
