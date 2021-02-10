771f1a2b98afb340822922fb597b2ffb67b662ae
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 16:47:13 -0800

    imports msw, adds rest and setupServe to mock API fetch in CharacterPages test

--- src/containers/CharacterPages.test.jsx
+++ src/containers/CharacterPages.test.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import CharacterPage from './CharacterPages';
 import { MemoryRouter } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import CharacterPage from './CharacterPages';
+
+const server = setupServer(
+  rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
+    return res(ctx.json());
+  })
+);
 
 describe('CharacterPages container', () => {
   it('displays a loading screen then a list of characters', async() => {
