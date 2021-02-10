58ea0ee62231db5dff113aaf1ec6f9a399626b15
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-09 19:47:24 -0800

    REFACTOR w/ CUSTOM HOOKS - move state to characters.js from CharacterPages.jsx

--- src/hooks/characters.js
+++ src/hooks/characters.js
@@ -1,5 +1,11 @@
 import { useEffect, useState } from 'react';
 
 export const useCharacters = () => {
-  
-}
+  const [loading, setLoading]  = useState(true);
+  const [characters, setCharacters] = useState([]);
+
+  return {
+    loading,
+    characters
+  };
+};
